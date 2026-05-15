61d7e47203d737bda89b87b600ebe7fe3531c523
alunoEtec <aula@etec.com>
2026-05-15 10:03:46 -0300

    ok

--- sistema/estoque.php
+++ sistema/estoque.php
@@ -47,6 +47,7 @@ if(isset($_POST['cadastrar_produto'])){
 
             mysqli_query($conexao,"
                 UPDATE estoque SET
+                    qtd = $qtd, 
                     qtd_minima = $qtd_minima
                 WHERE id_prod = $idProduto
             ");
@@ -344,6 +345,7 @@ function editarProduto(p){
     document.querySelector('[name="preco"]').value = p.preco;
     document.querySelector('[name="custo"]').value = p.custo;
     document.querySelector('[name="qtd_minima"]').value = p.qtd_minima;
+    document.querySelector('[name="qtd"]').value = p.qtd;
 
     document.querySelector('[name="categoria"]').value = p.id_cat;
     document.querySelector('[name="fornecedor"]').value = p.id_forn;
