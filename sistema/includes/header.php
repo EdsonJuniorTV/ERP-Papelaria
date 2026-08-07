88737c4c579976f89dcd8f7a3db27f7015d59cef
EdsonJuniorTV <edsongustavodiasjunior@gmail.com>
2026-08-07 10:48:23 -0300

    CSS melhor feito pelo Erick

--- sistema/includes/header.php
+++ sistema/includes/header.php
@@ -30,14 +30,14 @@ $nome_usuario = $_SESSION['user_nome'] ?? 'Usuário';
     <header class="top-header">
         <div class="main-header">
             <div class="logo">
-                <h2><span>✏️</span> Papelaria Central</h2>
+                <h2><span>📝</span> Papelaria Central</h2>
             </div>
             <nav class="nav-menu">
                 <ul>
                     <li><a href="dashboard.php">📊 Dashboard</a></li>
                     <li><a href="caixa.php">🛒 Caixa</a></li>
-                    <li><a href="configuracoes.php">(:P) Configurações</a></li>
-                    <li><a href="cadastrar_produto.php">(:3) Produtos</a></li>
+                    <li><a href="configuracoes.php">⚙️ Configurações</a></li>
+                    <li><a href="cadastrar_produto.php">✏️ Produtos</a></li>
                     <li>
                         <a href="estoque.php">
                             📦 Estoque 
@@ -51,7 +51,7 @@ $nome_usuario = $_SESSION['user_nome'] ?? 'Usuário';
                     <li><a href="cadastro.php">👥 Clientes</a></li>
                     <li><a href="fornecedores_gestao.php">🚚 Fornecedores</a></li>
                     <li><a href="funcionarios_gestao.php">👔 RH</a></li>
-                    <li><a href="financeiro.php">(D) Financeiro</a><li>
+                    <li><a href="financeiro.php">💵 Financeiro</a><li>
                 </ul>
             </nav>
             <div class="user-info">
